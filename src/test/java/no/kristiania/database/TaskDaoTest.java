a5bcf3432bedfff17f59538ba2bdcb30e7ab7fa4
Elise Easter <56083509+eliseeaster@users.noreply.github.com>
2020-11-02 10:55:28 +0100

    .

---
+++ src/test/java/no/kristiania/database/TaskDaoTest.java
@@ -0,0 +1,17 @@
+package no.kristiania.database;
+
+import org.junit.jupiter.api.Test;
+
+public class TaskDaoTest {
+
+    @Test
+    void shouldListAllTests(){
+
+    }
+
+    @Test
+    void shouldRetreiveAllCategoryProperties(){
+
+    }
+
+}
