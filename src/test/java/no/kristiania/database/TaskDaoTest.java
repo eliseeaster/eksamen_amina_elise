3ae7f20429584e3eba55232769ee61e29a673e66
Elise Easter <56083509+eliseeaster@users.noreply.github.com>
2020-11-03 19:13:26 +0100

    Revert "OBS! Had some problems with setName from push, tried to fix. New yellow test, shouldListAllTasks"
    This reverts commit 7d0314d5

--- src/test/java/no/kristiania/database/TaskDaoTest.java
+++ src/test/java/no/kristiania/database/TaskDaoTest.java
@@ -1,31 +1,12 @@
 package no.kristiania.database;
 
-import org.flywaydb.core.Flyway;
-import org.h2.jdbcx.JdbcDataSource;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 public class TaskDaoTest {
 
-    private EmployeeTaskDao taskDao;
-
-    @BeforeEach
-    void setUp() {
-        JdbcDataSource dataSource = new JdbcDataSource();
-        dataSource.setUrl("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1");
-        Flyway.configure().dataSource(dataSource).load().migrate();
-        taskDao = new EmployeeTaskDao(dataSource);
-    }
-
     @Test
-    void shouldListAllTasks(){
-        EmployeeTask task1 = exampleTask();
-        EmployeeTask task2 = exampleTask();
-        assertThat(taskDao.list())
-            .extracting(EmployeeTask::getName)
-            .contains(task1.getName(), task2.getName());
+    void shouldListAllTests(){
+
     }
 
     @Test
@@ -38,10 +19,6 @@ public class TaskDaoTest {
                 .usingRecursiveComparison()
                 .isEqualTo(task);
 
-    }
-
-    private EmployeeTask exampleTask() {
-    return null;
     }
 
 }
